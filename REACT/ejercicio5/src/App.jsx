c43f6d77e7e5b18480bef3db8bb7b69e746f7c64
Brais <103991765+BraisSO@users.noreply.github.com>
2023-01-12 16:14:42 +0100

    ejercicio 5 react

---
+++ REACT/ejercicio5/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Component1 from './Components/Component1'
+import Component2 from './Components/Component2'
+
+function App() {
+ 
+  return (
+   <>
+   <Component1 numero="676736637"/>
+   <Component2 numero="653453635"/>
+   </>
+  )
+}
+
+export default App
