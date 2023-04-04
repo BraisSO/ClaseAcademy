c7c5f0b25f4cc7d933ae9027af36721a8c7c7d00
Brais <103991765+BraisSO@users.noreply.github.com>
2023-04-04 13:03:17 +0200

    arquetipo finalizado

--- Python/arquetipo/main.py
+++ Python/arquetipo/main.py
@@ -2,8 +2,13 @@ from flask import Flask
 from envs.dev.dev_env import config, get_database_config
 from database.db import init_app
 from users.routes.user_routes import users_routes
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+#Cors config
+app.config['JSON_AS_ASCII'] = False
+CORS(app)
 
 #Database config
 user = get_database_config().get('MYSQL_USER')
--- Python/arquetipo/users/routes/user_routes.py
+++ Python/arquetipo/users/routes/user_routes.py
@@ -2,8 +2,38 @@ from flask import Blueprint
 from repo.user_repo import *
 from users.controller.user_controller import *
 
-users_routes = Blueprint('user_routes', __name__)
+users_routes = Blueprint('user_routes', __name__, url_prefix="/api")
 
-@users_routes.get("/")
-def home():
+#Lista de usuarios
+@users_routes.get("/users")
+def users_list():
     return users()
+
+
+#Usuarios por id
+@users_routes.get("/user/<int:id>")
+def users_by_id(id):
+    return user(id)
+
+#Usuarios por username
+@users_routes.get("/by-username/<string:username>")
+def users_by_username(username):
+    return by_username(username)
+
+
+#Guardar usuarios
+@users_routes.post("/save")
+def save():
+    return user_registry()
+
+#Modificar usuario
+@users_routes.put("/update/<int:id>")
+def update(id):
+    return update_user(id)
+
+
+#Eliminar usuario
+@users_routes.delete("/delete/<int:id>")
+def delete_user(id):
+    delete(id)
+    return "Perfect"
